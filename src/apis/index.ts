8d59c66847b7b11de7442a9c58f3f6bcdf83ed22
Ian Lord <lordi@ezmax.ca>
2020-09-04 03:16:19 -0400

    Release 1.0.11

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,6 +1,6 @@
 export * from './ActivesessionApi';
-export * from './EzsignWebhookApi';
 export * from './EzsigndocumentApi';
 export * from './EzsignfolderApi';
 export * from './EzsignfoldersignerassociationApi';
 export * from './EzsignsignatureApi';
+export * from './FranchisereferalincomeApi';
